b4bc4c624fb2542825980b5e83b924ec156de539
Merajul Hasan <merajulhasan09@gmail.com>
2024-11-16 15:39:04 +0600

    added jwt

--- gadget-shop-server/index.js
+++ gadget-shop-server/index.js
@@ -12,11 +12,8 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-// please check your own mongodb cluster and embed the only code after @cluster0.yourCode and put it in this uri and also change DB_USER,DB_PASS in .env file
-// Make it template string first and change the inside bracket() to {}.
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wov5hm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,14 +24,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
@@ -43,8 +37,14 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("server is running");
 });
-
+//  jwt
+app.post("/authentication", async (req, res) => {
+  const userEmail = req.body;
+  const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+  res.send({ token });
+});
 app.listen(port, () => {
-  // please change the inside bracket() to {}.
   console.log(`Server is running on port ${port}`);
 });
